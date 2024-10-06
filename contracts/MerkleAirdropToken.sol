8eb7845aaabdb0ea80ba5fa67b40a8788a57e1a5
nadine-ben-chekaya <nadinbenchekaya@gmail.com>
2024-10-06 09:47:04 +0100

    update sc and test commit

--- contracts/MerkleAirdropToken.sol
+++ contracts/MerkleAirdropToken.sol
@@ -7,11 +7,12 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleAirdropToken is ERC20, Ownable {
     bytes32 public merkleRoot; // Store the Merkle root
+    uint256 public immutable rewardAmount; // fix amount will be claimed for each participant
     mapping(address => bool) public claimed; // Keep track of who has claimed
 
-    constructor(address initialOwner) ERC20("MerkleToken", "MTKN")Ownable(initialOwner) {
-        
-        _mint(msg.sender, 1000000 * 10 ** 18); // Initial supply
+    constructor(address initialOwner, uint256 _rewardAmount) ERC20("MerkleToken", "MTKN")Ownable(initialOwner) {
+         _mint(msg.sender, 1000000 * 10 ** 18); // Initial supply
+        rewardAmount = _rewardAmount;
     }
 
     // Set the new Merkle root for the current airdrop
@@ -20,17 +21,16 @@ contract MerkleAirdropToken is ERC20, Ownable {
     }
 
     // Function to claim tokens
-    function claimTokens(uint256 amount, bytes32[] calldata merkleProof) external {
+    function claimTokens(bytes32[] calldata merkleProof) external {
         require(!claimed[msg.sender], "Tokens already claimed");
 
-        // Compute the leaf node (address + amount)
-        bytes32 leaf = keccak256(abi.encodePacked(msg.sender, amount));
-
+        // Compute the leaf node (address)
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
         // Verify the proof
         require(MerkleProof.verify(merkleProof, merkleRoot, leaf), "Invalid proof");
 
         claimed[msg.sender] = true; // Mark as claimed
 
-        _transfer(owner(), msg.sender, amount); // Transfer the tokens
+        _transfer(owner(), msg.sender, rewardAmount); // Transfer the tokens
     }
 }
--- test/MerkleAirdropTest.js
+++ test/MerkleAirdropTest.js
@@ -4,31 +4,30 @@ const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
 
 describe("Merkle Airdrop Token", function () {
-  let token, owner, addr1, addr2;
+  let token, owner, addr1, addr2, addr3, addr4, addr5;
   let merkleTree, root;
+  const RewardAmount = ethers.parseEther("100");
 
   beforeEach(async function () {
     // Deploy the MerkleAirdropToken contract
-    const MerkleAirdropToken = await ethers.getContractFactory(
-      "MerkleAirdropToken"
-    );
-    [owner, addr1, addr2] = await ethers.getSigners();
-    token = await ethers.deployContract("MerkleAirdropToken", [owner]);
+    [owner, addr1, addr2, addr3, addr4, addr5] = await ethers.getSigners();
+
+    token = await ethers.deployContract("MerkleAirdropToken", [
+      owner,
+      RewardAmount,
+    ]);
     await token.waitForDeployment();
     console.log("contreact address=", token.target);
 
     // Airdrop participants and their amounts
     const participants = [
-      { address: addr1.address, amount: ethers.parseEther("100") },
-      { address: addr2.address, amount: ethers.parseEther("200") },
+      addr1.address,
+      addr2.address,
+      addr3.address,
+      addr4.address,
     ];
-
     // Generate leaves by hashing the participants' address + amount
-    const leaves = participants.map((x) =>
-      keccak256(
-        ethers.solidityPacked(["address", "uint256"], [x.address, x.amount])
-      )
-    );
+    const leaves = participants.map((x) => keccak256(x));
 
     // Create the Merkle Tree and get the root
     merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
@@ -39,53 +38,43 @@ describe("Merkle Airdrop Token", function () {
   });
 
   it("1.Should allow valid participants to claim tokens", async function () {
-    // addr1 wants to claim 100 tokens
-    const amount = ethers.parseEther("100");
-    const leaf = keccak256(
-      ethers.solidityPacked(["address", "uint256"], [addr1.address, amount])
-    );
+    // addr1 wants to claim
+    const leaf = keccak256(addr1.address);
     const proof = merkleTree.getHexProof(leaf);
     // Claim tokens
-    await token.connect(addr1).claimTokens(amount, proof);
+    await token.connect(addr1).claimTokens(proof);
     // Check the balance of addr1
-    expect(await token.balanceOf(addr1.address)).to.equal(amount);
+    expect(await token.balanceOf(addr1.address)).to.equal(RewardAmount);
   });
 
-  it("2.Should prevent double claiming", async function () {
-    // addr1 claims 100 tokens
-    const amount = ethers.parseEther("100");
-    const leaf = keccak256(
-      ethers.solidityPacked(["address", "uint256"], [addr1.address, amount])
-    );
+  it("2.Should not prevent double claiming", async function () {
+    // addr1 claims double
+    const leaf = keccak256(addr1.address);
     const proof = merkleTree.getHexProof(leaf);
     // First claim
-    await token.connect(addr1).claimTokens(amount, proof);
+    await token.connect(addr1).claimTokens(proof);
     // Trying to claim again should fail
-    await expect(
-      token.connect(addr1).claimTokens(amount, proof)
-    ).to.be.revertedWith("Tokens already claimed");
+    await expect(token.connect(addr1).claimTokens(proof)).to.be.revertedWith(
+      "Tokens already claimed"
+    );
   });
 
   it("3.Should reject claims with an invalid proof", async function () {
-    // addr1 wants to claim 100 tokens with an invalid proof
+    // addr1 wants to claim with an invalid proof
     const invalidProof = [];
-    const amount = ethers.parseEther("100");
     // This should fail due to invalid proof
     await expect(
-      token.connect(addr1).claimTokens(amount, invalidProof)
+      token.connect(addr1).claimTokens(invalidProof)
     ).to.be.revertedWith("Invalid proof");
   });
 
   it("4.Should reject claims for non-participants", async function () {
-    // addr2 claims 300 tokens (not part of the airdrop)
-    const amount = ethers.parseEther("300");
-    const leaf = keccak256(
-      ethers.solidityPacked(["address", "uint256"], [addr2.address, amount])
-    );
+    // addr5 claims (not part of the airdrop)
+    const leaf = keccak256(addr5.address);
     const proof = merkleTree.getHexProof(leaf);
     // This should fail because addr2 is not in the list
-    await expect(
-      token.connect(addr2).claimTokens(amount, proof)
-    ).to.be.revertedWith("Invalid proof");
+    await expect(token.connect(addr2).claimTokens(proof)).to.be.revertedWith(
+      "Invalid proof"
+    );
   });
 });
